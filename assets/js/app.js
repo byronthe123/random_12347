ead6c4ab9483b21e3e03380b123f67218ecac08c
Aedirn <byroninjeeli@gmail.com>
2018-11-07 09:42:48 -0500

    commit 9:42 AM 11/7/19

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,22 +17,28 @@ const db = firebase.database();
 //Login/User Authentication: --------------------------------------------------------------------------------------------------
 
       // FirebaseUI config.
-      var uiConfig = {
+    var uiConfig = {
         signInSuccessUrl: `https://byronthe123.github.io/random_12347/main.html`,
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.GithubAuthProvider.PROVIDER_ID,
         ],
+        'callbacks': {
+            'signInSuccess': function(currentUser, credential, redirectUrl) {
+                window.location.assign('https://byronthe123.github.io/random_12347/main.html');
+                return true;
+            }
+        }
         // tosUrl and privacyPolicyUrl accept either url string or a callback
         // function.
         // Terms of service url/callback.
-        tosUrl: '<your-tos-url>',
-        // Privacy policy url/callback.
-        privacyPolicyUrl: function() {
-          window.location.assign('<your-privacy-policy-url>');
-        }
-      };
+        // tosUrl: '<your-tos-url>',
+        // // Privacy policy url/callback.
+        // privacyPolicyUrl: function() {
+        //   window.location.assign('<your-privacy-policy-url>');
+        // }
+    };
 
       // Initialize the FirebaseUI Widget using Firebase.
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
