6b985054fdde965eadc204b71ae1749f5e49e354
Aedirn <byroninjeeli@gmail.com>
2018-11-11 13:47:28 -0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -257,7 +257,7 @@ $(document).on('click', '.fa-sync-alt', function() {
     alert('update clicked');
     console.log($(this).parent());
     console.log($(this).parent().prevObject[0]);
-    let id = ($(this).parent().prevObject[0].id);
+    let id = ($(this).parent().parent());
     console.log(manuallyUpdate(id));
-    $(this).parent().prevObject[0].replaceWith(manuallyUpdate(id));
+    // $(this).parent().prevObject[0].replaceWith(manuallyUpdate(id));
 });
